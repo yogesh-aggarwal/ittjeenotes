e88c86ae6ee9fab2b4e4a8aec8e565446addab15
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-09 13:28:12 +0530

    Added chats screen

--- # Expertize/exp1/lib/main.dart
+++ # Expertize/exp1/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
+import 'package:scratcher/scratcher.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 void main() {
   runApp(Home());
@@ -9,56 +12,40 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with TickerProviderStateMixin {
-  AnimationController controller;
-  Animation animation;
-
-  double radius = 20;
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 2),
-    );
-    controller.addListener(() {
-      setState(() {});
-      if (controller.isCompleted) {
-        controller.forward();
-      } else if (controller.isDismissed) {
-        controller.reverse();
-      }
-    });
-    animation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.ease,
-    );
-    animation = Tween(begin: 20.0, end: 40.0).animate(animation);
-  }
+class _HomeState extends State<Home> {
+  final scratchKey = GlobalKey<ScratcherState>();
 
   @override
   Widget build(BuildContext context) {
+    double value = 20;
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Hello World!")),
-          elevation: 0,
-        ),
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(
-                double.parse(animation.value),
-              ),
-              color: Colors.green,
-            ),
-            width: 200,
-            height: 200,
+          appBar: AppBar(
+            title: Center(child: Text("Hello World!")),
+            elevation: 0,
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              SleekCircularSlider(
+                min: 0,
+                max: 1000,
+                initialValue: value,
+                appearance: CircularSliderAppearance(),
+                onChange: (double value) {
+                  // print(value);
+                },
+              ),
+              OutlineButton.icon(
+                onPressed: () {
+                  setState(() {
+                    value = 500;
+                  });
+                },
+                icon: Icon(Icons.send),
+                label: Text("Start"),
+              )
+            ],
+          )),
     );
   }
 }
